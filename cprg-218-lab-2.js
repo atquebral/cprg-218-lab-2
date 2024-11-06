d1505f87f80bea2df270b1481649791dca2fa6ed
atquebral <ana.quebral@edu.sait.ca>
2024-11-06 16:55:27 -0700

    buttons
    made buttons function

---
+++ cprg-218-lab-2.js
@@ -0,0 +1,29 @@
+sheep.addEventListener("click", function(){
+    console.log("sheep with horns")
+    document.getElementById('sheep').src='images/sheep.webp';
+    
+})
+
+bear.addEventListener("click", function(){
+    console.log("fuzzy bear")
+    document.getElementById('bear').src='images/bear.webp';
+    
+})
+
+moose.addEventListener("click", function(){
+    console.log("elliot")
+    document.getElementById('moose').src='images/moose.webp';
+    
+})
+
+wolf.addEventListener("click", function(){
+    console.log("woof woof")
+    document.getElementById('wolf').src='images/wolf.webp';
+    
+})
+
+elk.addEventListener("click", function(){
+    console.log("huh")
+    document.getElementById('elk').src='images/elk.webp';
+    
+})
